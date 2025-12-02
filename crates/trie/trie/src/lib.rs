20fd4040892ccabb26990525de66621015385ff0
cliff0412 <yangweitaohustntu@gmail.com>
2025-12-02 12:13:50 +0800

    add triedb

--- crates/trie/trie/src/lib.rs
+++ crates/trie/trie/src/lib.rs
@@ -69,3 +69,6 @@ pub mod mock;
 
 /// Verification of existing stored trie nodes against state data.
 pub mod verify;
+
+mod trie_ext;
+pub use trie_ext::{StateRootTrieDb, TrieExtDatabase};
---
+++ crates/trie/trie/src/trie_ext.rs
@@ -0,0 +1,91 @@
+use std::path::Path;
+use std::time::Instant;
+use alloy_primitives::B256;
+use alloy_trie::{HashBuilder, EMPTY_ROOT_HASH};
+use tracing::{debug, trace};
+use reth_execution_errors::StateRootError;
+use reth_trie_common::{prefix_set::TriePrefixSets};
+use crate::{IntermediateStateRootState, StateRoot, StateRootProgress, StorageRoot};
+use crate::hashed_cursor::{HashedCursor, HashedCursorFactory};
+use crate::node_iter::{TrieElement, TrieNodeIter};
+use crate::stats::TrieTracker;
+use crate::trie::StateRootContext;
+use crate::trie_cursor::TrieCursorFactory;
+use crate::walker::TrieWalker;
+use triedb::{Database as TrieDbDatabase, path::{AddressPath, StoragePath}, };
+use nybbles::Nibbles;
+use triedb::account::Account as TrieDbAccount;
+use alloy_consensus::constants::KECCAK_EMPTY;
+#[derive(Debug)]
+pub struct TrieExtDatabase {
+    pub inner: TrieDbDatabase,
+}
+
+
+impl TrieExtDatabase {
+    pub fn new(db_path: impl AsRef<Path>) -> Self {
+        let db_path = db_path.as_ref();
+        let db = TrieDbDatabase::create_new(db_path).unwrap();
+        Self {
+            inner: db,
+        }
+    }
+}
+
+
+/// `StateRoot` is used to compute the root node of a state trie.
+#[derive(Debug)]
+pub struct StateRootTrieDb<H> {
+    /// The factory for hashed cursors.
+    pub hashed_cursor_factory: H,
+    pub db: TrieExtDatabase
+}
+
+
+impl<H> StateRootTrieDb<H> {
+    /// Creates [`StateRootTrieDb`] with
+    pub fn new(hashed_cursor_factory: H, db: TrieExtDatabase) -> Self {
+        Self {
+            hashed_cursor_factory,
+            db
+        }
+    }
+}
+impl<H> StateRootTrieDb<H>
+where
+    H: HashedCursorFactory + Clone,
+{
+    pub fn calculate_commit(self) -> Result<B256, StateRootError> {
+        trace!(target: "trie::state_root", "calculating state root");
+        let mut acct_cursor = self.hashed_cursor_factory.hashed_account_cursor()?;
+        let mut tx = self.db.inner.begin_rw().unwrap();
+        let mut account_entry = acct_cursor.next().unwrap();
+        while let Some((hashed_address, account)) = account_entry {
+            let nibbles = Nibbles::unpack(hashed_address);
+            let address_path = AddressPath::new(nibbles);
+            
+            let triedb_account = TrieDbAccount {
+                nonce: account.nonce,
+                balance: account.balance,
+                code_hash: account.bytecode_hash.unwrap_or(KECCAK_EMPTY),
+                storage_root: EMPTY_ROOT_HASH,
+            };
+            tx.set_account(address_path.clone(), Some(triedb_account)).unwrap();
+
+let mut storage_cursor = self.hashed_cursor_factory.hashed_storage_cursor(hashed_address)?;
+
+              let mut storage_entry = storage_cursor.seek(B256::ZERO)?;
+            while let Some((hashed_storage_key, storage_value)) = storage_entry {
+                let storage_path = StoragePath::for_address_path_and_slot_hash(address_path.clone(), Nibbles::unpack(hashed_storage_key));
+                tx.set_storage_slot(storage_path, Some(storage_value)).unwrap();
+
+                storage_entry = storage_cursor.next()?;
+            }
+
+
+            account_entry = acct_cursor.next()?;
+        }
+        tx.commit().unwrap();
+        Ok(self.db.inner.state_root())
+    }
+}
